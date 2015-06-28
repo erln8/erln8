74f70e9a3a4231870926b886b06f6e90e6e211fd
Dave Parfitt <dparfitt@chef.io>
2015-06-28 17:38:40 -0400

    cleanup build for new repo

--- binary_gen/ubuntu1404/build.sh
+++ binary_gen/ubuntu1404/build.sh
@@ -1,12 +1,12 @@
 mkdir -p ./erln8
 docker build -t reo/builder_1404 .
 docker run -i -v ${PWD}/erln8:/erln8 reo/builder_1404 << COMMANDS
-git clone https://github.com/erln8/reo.git
-cd /reo
+git clone https://github.com/erln8/erln8.git
+cd /erln8
 dub build
-cp /reo/reo /erln8/erln8
-cp /reo/reo /erln8/reo
-cp /reo/reo /erln8/reo3
+cp /erln8/reo /erln8/erln8
+cp /erln8/reo /erln8/reo
+cp /erln8/reo /erln8/reo3
 COMMANDS
 cp ../../LICENSE ./erln8/
 cp ../install.sh ./erln8/
