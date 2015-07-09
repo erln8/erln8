575d50a19d6d3733a86ba609e5a80dddfd35706a
Dave Parfitt <dparfitt@chef.io>
2015-07-09 06:52:04 -0400

    install.sh clones default repos now

--- binary_gen/install.sh
+++ binary_gen/install.sh
@@ -3,3 +3,6 @@ mkdir -p ~/.erln8.d/bin
 cp erln8 ~/.erln8.d/bin/erln8
 cp erln8 ~/.erln8.d/bin/reo
 cp erln8 ~/.erln8.d/bin/reo3
+~/.erln8.d/bin/erln8 --clone default
+~/.erln8.d/bin/reo --clone default
+~/.erln8.d/bin/reo3 --clone default
