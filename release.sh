96a16f14eea2deed3e385925ed304a52c1b7f3a0
Dave Parfitt <dparfitt@chef.io>
2015-07-15 10:18:08 -0400

    fixed release script to commit and push version change

--- release.sh
+++ release.sh
@@ -4,7 +4,10 @@ perl -p -i -e 's/^((\d+\.)*)(\d+)(.*)$/$1.($3+1).$4/e' ./src/VERSION
 next_version=`cat ./src/VERSION`
 echo Generating $next_version
 echo "const ERLN8_VERSION=\"${next_version}\";" > ./src/version_gen.d
+git add ./src/version_gen.d
+git commit -m "bumped versioon to ${next_version}"
 git tag $next_version
+git push origin master
 git push origin --tags
 make docs
 cd ./binary_gen && make
